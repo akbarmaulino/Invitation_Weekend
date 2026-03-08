6c38ede94c57801422901906b244be691b57a2c6
Yantrisnandra Akbar Maulino <maulinoakbar8@gmail.com>
2026-03-08 13:04:23 +0700

    update navbar

--- .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[next]_internal_font_google_geist_9e050971_module_9965b6e6.css",
-  "static/chunks/node_modules_cd4f68f3._.js",
-  "static/chunks/_734cd088._.js"
-],
-    source: "dynamic"
-});
